8dd0d80b0d771e634b0601c397545da81d64f2d7
Rangel Reale <rangelspam@gmail.com>
2024-12-14 08:27:05 -0300

    context

---
+++ slog.go
@@ -0,0 +1,38 @@
+package panyl
+
+import (
+	"context"
+	"log/slog"
+)
+
+type slogContextKey string
+
+const (
+	// LoggerCtxKey is the string used to extract logger
+	slogLoggerCtxKey slogContextKey = "logger"
+)
+
+var emptySLogger *slog.Logger
+
+func SLogToContext(ctx context.Context, logger *slog.Logger) context.Context {
+	return context.WithValue(ctx, slogLoggerCtxKey, logger)
+}
+
+func SLogFromContext(ctx context.Context) *slog.Logger {
+	v, ok := ctx.Value(slogLoggerCtxKey).(*slog.Logger)
+	if ok {
+		return v
+	}
+	return emptySLogger
+}
+
+func init() {
+	emptySLogger = slog.New(&discardHandler{})
+}
+
+type discardHandler struct{}
+
+func (dh discardHandler) Enabled(context.Context, slog.Level) bool  { return false }
+func (dh discardHandler) Handle(context.Context, slog.Record) error { return nil }
+func (dh discardHandler) WithAttrs(attrs []slog.Attr) slog.Handler  { return dh }
+func (dh discardHandler) WithGroup(name string) slog.Handler        { return dh }
